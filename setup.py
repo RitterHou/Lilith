d6f4d6aaddb79ffaa5848af661920f49a7a6cfeb
侯锐 <hourui@qianmi.com>
2017-08-04 22:09:45 +0800

    完成基础功能

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
 __author__ = 'derobukal'
 __time__ = '2017/8/2 23:14'
 
 setup(
-    name='tqrcode',
+    name='lilith',
     version='0.0.1',
-    url='http://nosuchfield.com',
+    url='https://github.com/RitterHou/Lilith',
     author='derobukal',
     author_email='derobukal@gmail.com',
     description='show qrcode in terminal',
     license='GPL3',
-    packages=['tqrcode'],
+    packages=['lilith'],
     entry_points={
         'console_scripts': [
-            'tqrcode=tqrcode.main:show'
+            'lilith=lilith.main:show'
         ],
     },
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: GPL3 License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
     install_requires=[
         'qrcode>=5.3',
